5e49522c789401582ed226367bb71f58c5cbd839
Deep-0310 <108914749+Deep-0310@users.noreply.github.com>
2024-10-06 13:15:04 +0000

    datatype

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3) //code redability should be high
+ console.log("Deep")
+
+ let name= "Deep"
+ let age= 18
+ let isLoggedIn=false
+ let state;
+
+
+ //number => 2 to power 53
+ //bigint
+ //string=> "",''
+ //boolean => true/false
+ //null
+ //undefined
+ //symbol => unique
+
+ //object
+
+
+ //typeof null is object and for undefined is undefined
+ console.log(typeof undefined)
