c0d14d681dd0ffc796c0d62190bbe491dcea3d9b
Deep-0310 <108914749+Deep-0310@users.noreply.github.com>
2024-11-23 09:34:35 +0000

    Conversion

---
+++ 01_Basics/01_var.js
@@ -0,0 +1,12 @@
+const accountId=52255;
+let accountMail="a@gmail.com"
+var accountPass="1235"
+
+accountCity="Jaipur"
+
+//accountId=2
+
+ 
+
+console.log(accountId);
+console.table([accountId,accountMail,accountPass,accountCity])
---
+++ 01_Basics/03_Convert.js
@@ -0,0 +1,22 @@
+let score ="hitesh"
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" =>33
+// "33ac" =>NaN
+// true =>1,false=>0
+
+let isLoggedIn="deep"
+let booleanIsLoggedin = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedin);
+
+
+// 1 => true , 0=>false
+//"" => false
+//"deep" => true
+
+let someNumber = 33
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof (stringNum));
